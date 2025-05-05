a9d305a514300d41e805370a2d0c698c02c1eee6
Busra Dovan <devdovanbusra@outlook.com>
2025-05-05 18:57:51 +0300

    Add project files.

---
+++ MyRocketProject/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace MyRocketProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "My Rocket Project";
+
+            string rocket = "     |\r\n     |\r\n    / \\\r\n   / _ \\\r\n  |.o '.|\r\n  |'._.'|\r\n  |     |\r\n ,'|  | |`.\r\n/  |  | |  \\\r\n|,-'--|--'-.|";
+
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine("\n" + new string('*', 30) + "My Rocket Project" + new string('*', 30) + "\n");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("It is " + i);
+                rocket = "\r\n" + rocket;
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(rocket);
+                Console.ForegroundColor= ConsoleColor.Yellow;
+                Console.Beep();
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("The Rocket has Landed!");
+            Console.ReadKey();
+        }
+    }
+}
